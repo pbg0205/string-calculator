70d69682baaa734d65b131c171112bbb9fb37d5a
pbg0205 <pbg0205@naver.com>
2020-09-20 06:17:17 +0900

    [테스트] 테스트 메소드 명칭 변경

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -8,31 +8,31 @@ public class CalculatorTest {
     Calculator calculator;
 
     @Test
-    void addTest() {
-        calculator = new Calculator(new String[]{"2", "+", "3"});
-        assertThat(calculator.calculate()).isEqualTo(5);
+    void 덧셈() {
+        calculator = new Calculator(new String[]{"2", "+", "3.5"});
+        assertThat(calculator.calculate()).isEqualTo(5.5);
     }
 
     @Test
-    void subtractTest() {
-        calculator = new Calculator(new String[]{"2", "-", "3"});
-        assertThat(calculator.subtract(2, 3)).isEqualTo(-1);
+    void 뺄셈() {
+        calculator = new Calculator(new String[]{"2", "-", "3.5"});
+        assertThat(calculator.subtract(2, 3.5)).isEqualTo(-1.5);
     }
 
     @Test
-    void multiplyTest() {
-        calculator = new Calculator(new String[]{"2", "*", "3"});
-        assertThat(calculator.multiply(2, 3)).isEqualTo(6);
+    void 곱셈() {
+        calculator = new Calculator(new String[]{"2.0", "*", "3.0"});
+        assertThat(calculator.multiply(2.0, 3.0)).isEqualTo(6.0);
     }
 
     @Test
-    void divideTest() {
-        calculator = new Calculator(new String[]{"2", "/", "3"});
-        assertThat(calculator.divide(2, 3)).isEqualTo(0);
+    void 나눗셈() {
+        calculator = new Calculator(new String[]{"2.0", "/", "3.0"});
+        assertThat(calculator.divide(2.0, 3.0)).isEqualTo(0.5);
     }
 
     @Test
-    void 정상적인_입력() {
+    void normalInputTest() {
         String testInput = "2 + 3 * 4 / 2";
         Calculator cal = new Calculator(testInput.split(" "));
         assertThat(cal.calculate()).isEqualTo(10);
